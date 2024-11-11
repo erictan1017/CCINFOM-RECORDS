d078325f9d46bc6ce7a7b86216bb9a0d6963ca23
Luis-AQR <luis_roa@dlsu.edu.ph>
2024-11-11 21:18:12 +0800

    Update restaurantres.sql

--- restaurantres.sql
+++ restaurantres.sql
@@ -34,8 +34,25 @@ CREATE TABLE `products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `staffs`
+--
+
+DROP TABLE IF EXISTS `staffs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staffs` (
+  `staff_ID` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `age` int NOT NULL,
+  `working_Day` date NOT NULL,
+  `salary` float NOT NULL,
+  PRIMARY KEY (`staff_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
 -- Table structure for table `reservations`
 --
+
 DROP TABLE IF EXISTS `reservations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -61,6 +78,17 @@ LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
 INSERT INTO `products` VALUES (1,'carrot',20,15,'No'),(2,'mango shake',150,10,'Yes'),(3,'beef shank',600,7,'No'),(4,'veal',650,5,'No'),(5,'salmon',700,10,'No'),(6,'osso buco',850,4,'Yes'),(7,'mashed potato',200,5,'Yes'),(8,'french onion soup',350,9,'Yes'),(9,'minestrone',300,9,'Yes'),(10,'tomato',100,15,'No');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- Dumping data for table `staffs`
+--
+
+LOCK TABLES `staffs` WRITE;
+/*!40000 ALTER TABLE `staffs` DISABLE KEYS */;
+INSERT INTO `staffs` VALUES (1, 'John Doe', 28, '2024-11-15', 3000.00), (2, 'Jane Doe', 34, '2024-11-20', 3200.00), (3, 'Luis Roa', 26, '2024-11-25', 2800.00), (4, 'Alice Johnson', 30, '2024-12-01', 3100.00), (5, 'Chris Lee', 40, '2024-12-05', 3500.00);
+/*!40000 ALTER TABLE `staffs` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
